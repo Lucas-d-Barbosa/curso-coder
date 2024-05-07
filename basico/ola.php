7a4260b7d969eefeb93b6ea8bde7df73d37fbfb8
Lucas Barbosa <francisco.lucas.d.santos.barbosa@gmail.com>
2024-05-07 10:51:29 -0300

    Criei o ola.php

---
+++ basico/ola.php
@@ -0,0 +1,13 @@
+<div class="titulo">Olá, PHP!</div>
+
+<h2>Exemplo A</h2>
+<?php
+echo "Olá, mundo!<br>Olá, PHP!";
+
+?>
+<h2>Exemplo B</h2>
+<?= Date('d/m/y')?>
+
+<h2>PHP info</h2>
+<?php
+phpinfo();
