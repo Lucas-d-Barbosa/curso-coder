c1c4faeaa525c9190f1a10748cab4532d826b53b
Lucas Barbosa <francisco.lucas.d.santos.barbosa@gmail.com>
2024-05-13 15:09:59 -0300

    alterado

--- variaveis/desafio_variaveis_variaveis.php
+++ variaveis/desafio_variaveis_variaveis.php
@@ -7,6 +7,7 @@ $Eu = 'consegui';
 $consegui = 'responder';
 $responder = 'esse';
 $esse = 'desafio';
-
+echo "Nossa! Eu consegui responder esse desafio.";
+echo '<br>';
 echo "{$a}! {$$a} {$$$a} {$$$$a} {$$$$$a} {$$$$$$a}.";
 ?>
