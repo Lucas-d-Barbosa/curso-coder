4c60b72d671cbdc9f91e67988c56328288a291de
Lucas Barbosa <francisco.lucas.d.santos.barbosa@gmail.com>
2024-06-01 12:12:15 -0300

    Traits #02

--- index.php
+++ index.php
@@ -64,6 +64,10 @@
                             <a href="exercicio.php?dir=classes_objetos&file=traits_01">Traits #01</a>
                         </li>
 
+                        <li>
+                            <a href="exercicio.php?dir=classes_objetos&file=traits_02">Traits #02</a>
+                        </li>
+
 
                       
                                           
